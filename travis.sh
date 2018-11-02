f3a9510686a27524cf03619ec08ea0c770216174
Erik Martin-Dorel <erik@martin-dorel.org>
2018-11-02 11:58:01 +0100

    Fix broken link & s/$@/$*/

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 # This bash file is to be sourced at login and must have .sh extension
 
 # Excerpt from:
-# https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/templates/header.sh
+# https://github.com/travis-ci/travis-build/blob/d5760f7925d62fcad1737c02dc6486387aa8b432/lib/travis/build/templates/header.sh
 
 ANSI_RED="\033[31;1m"
 ANSI_GREEN="\033[32;1m"
@@ -15,14 +15,14 @@ travis_retry() {
   local count=1
   while [ $count -le 3 ]; do
     [ $result -ne 0 ] && {
-      echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
+      echo -e "\n${ANSI_RED}The command \"$*\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
     }
     "$@" && { result=0 && break; } || result=$?
     count=$(($count + 1))
     sleep 1
   done
   [ $count -gt 3 ] && {
-    echo -e "\n${ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
+    echo -e "\n${ANSI_RED}The command \"$*\" failed 3 times.${ANSI_RESET}\n" >&2
   }
   return $result
 }
